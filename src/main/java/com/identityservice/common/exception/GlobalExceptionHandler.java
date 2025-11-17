e0ae294e3cd098ff4c54cf368a5bbb70ccbf0b51
Shun <ngokinh30311@gmail.com>
2025-11-17 22:01:33 +0700

    demo conflict

--- src/main/java/com/identityservice/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/identityservice/common/exception/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
   /**
-   * Handle unexpected RuntimeExceptions (uncaught system errors). (new2)
+   * Handle unexpected RuntimeExceptions (uncaught system errors).
    *
    * @param ex the exception thrown
    * @return standardized API error response
